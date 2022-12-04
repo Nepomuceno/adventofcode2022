da74fde31adb2adad53d08e93e5c2ebf34b4487c
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2022-12-04 04:08:42 +0000

    Day 4 part 1

--- src/day04.rs
+++ src/day04.rs
@@ -1,3 +1,19 @@
-pub fn run(_input: &str) -> String {
-    "Not implemented yet".to_string()
+use std::collections::VecDeque;
+
+pub fn run(input: &str) -> String {
+    let mut overlap = 0;
+    for line in input.lines() {
+        let pair = line.split(',')
+        .map(|x| x.split('-')
+                        .map(|f| f.parse::<u32>().unwrap()).collect::<VecDeque<_>>())
+        .collect::<VecDeque<_>>();
+        if pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]
+        {
+            overlap += 1;
+        } else if pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1] {
+            overlap += 1;
+        }
+        println!("{overlap}")
+    }
+    overlap.to_string()
 }
