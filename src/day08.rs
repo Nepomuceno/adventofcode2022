df9f82171966b0c9d9efa682c29c22cd8dacb5bd
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2022-12-08 04:12:29 +0000

    Day 8 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -1,55 +1,45 @@
 use std::{collections::VecDeque};
 
-fn is_visible(grid: &VecDeque<VecDeque<u32>>, x: usize, y: usize) -> bool {
+fn scenic_score(grid: &VecDeque<VecDeque<u32>>, x: usize, y: usize) -> u32 {
     let content = grid[x][y];
-    // return true for the ones in the corners
     if x == 0 || y == 0 || x == grid.len() - 1 || y == grid[x].len() - 1 {
-        return true;
-    }
-    // return false for smallest ones
-    if content == 0 {
-        return false;
+        return 0;
     }
     // check the 4 directions
+    let mut up = 0;
+    let mut down = 0;
+    let mut left = 0;
+    let mut right = 0;
+    
     // up
-    for i in 0..x+1 {
-        
-        if i == x {
-            return true;
-        }
-        let val = grid[i][y];
-        if val >= content {
+    for i in (0..x).rev() {
+        up += 1;
+        if grid[i][y] >= content {
             break;
         }
     }
     // down
-    for i in x+1..grid.len()+1 {
-        if i == grid.len() {
-            return true;
-        }
+    for i in x+1..grid.len() {
+        down += 1;
         if grid[i][y] >= content {
             break;
         }
     }
     // left
-    for i in 0..y+1 {
-        if i == y {
-            return true;
-        }
+    for i in (0..y).rev() {
+        left += 1;
         if grid[x][i] >= content {
             break;
         }
     }
     // right
-    for i in y+1..grid[x].len()+1 {
-        if i == grid[x].len() {
-            return true;
-        }
+    for i in y+1..grid[x].len() {
+        right += 1;
         if grid[x][i] >= content {
             break;
         }
     }
-    return false;
+    return up*down*left*right;
 }
 
 pub fn run(input: &str) -> String {
@@ -60,17 +50,17 @@ pub fn run(input: &str) -> String {
             .map(|c| c.to_digit(10).unwrap())
                 .collect::<VecDeque<_>>())
         .collect::<VecDeque<_>>();
-    let mut visible = 0;
+    let mut max_score = 0;
     for i in 0..grid.len() {
         for j in 0..grid[i].len() {
-            let x_visible = is_visible(&grid, i, j);
-            if x_visible {
-                visible += 1;
+            let score = scenic_score(&grid, i, j);
+            if score > max_score {
+                max_score = score;
             }
-            print!("{}", if x_visible { "X" } else { " " })
+            print!("{} ", score)
         }
         println!("");
     }
-    return visible.to_string();
+    return max_score.to_string();
 }
 
