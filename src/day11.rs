aaac38d26f6dd24928d19e55e5d6e1decb2c31bc
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2022-12-11 08:01:40 +0000

    Day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -2,6 +2,7 @@ use std::{collections::{VecDeque}};
 
 #[derive(Debug)]
 #[derive(PartialEq)]
+#[derive(Clone)]
 enum Operation {
     Multiply,
     Add,
@@ -9,13 +10,14 @@ enum Operation {
     Squares,
 }
 #[derive(Debug)]
+#[derive(Clone)]
 struct Monkey {
     name: String,
-    test: i32,
+    test: u32,
     true_index: i32,
     false_index: i32,
     operation: Operation,
-    operand: i32,
+    operand: u32,
 }
 
 impl Monkey {
@@ -34,11 +36,11 @@ impl Monkey {
             };
         let operand = 
             if operation == Operation::Multiply || operation == Operation::Add {
-                lines[2].split(" ").last().unwrap().parse::<i32>().unwrap()
+                lines[2].split(" ").last().unwrap().parse::<u32>().unwrap()
             } else {
                 0
             };
-        let test = lines[3].replace("Test: divisible by ", "").trim().parse::<i32>().unwrap();
+        let test = lines[3].replace("Test: divisible by ", "").trim().parse::<u32>().unwrap();
         let true_index = lines[4].replace("If true: throw to monkey ", "").trim().parse::<i32>().unwrap();
         let false_index = lines[5].replace("If false: throw to monkey ", "").trim().parse::<i32>().unwrap();
         println!("{:?}", (name, test, true_index, false_index, &operation, operand));
@@ -51,16 +53,21 @@ impl Monkey {
             operand: operand,
         }
     }
-    fn get_items(content: &str) -> VecDeque<i32> {
+    fn get_items(content: &str, monkeys: &VecDeque<Monkey>) -> VecDeque<VecDeque<u32>> {
         let lines = content.lines().collect::<VecDeque<&str>>();
         lines[1]
             .split_once(":").unwrap().1.split(",")
             .map(
                 |x| {
-                    x.trim().parse::<i32>().unwrap()
+                    let number = x.trim().parse::<u32>().unwrap();
+                    let mut result = VecDeque::new();
+                    for monkey in monkeys {
+                        result.push_back(number % monkey.test);
+                    }
+                    result
                 }
             )
-            .collect::<VecDeque<i32>>()
+            .collect::<VecDeque<VecDeque<u32>>>()
     }
 
         
@@ -68,45 +75,47 @@ impl Monkey {
 
 pub fn run(input: &str) -> String {
     let monkeys_input = input.split("\n\n").collect::<Vec<&str>>();
-    let monkeys = monkeys_input.iter().map(|x| Monkey::new(x)).collect::<Vec<Monkey>>();
+    let monkeys = monkeys_input.iter().map(|x| Monkey::new(x)).collect::<VecDeque<Monkey>>();
     let mut monkey_items = VecDeque::new();
     let mut monkeys_busines = VecDeque::new();
     for monkey_input in monkeys_input {
-        monkey_items.push_back(Monkey::get_items(monkey_input));
+        monkey_items.push_back(Monkey::get_items(monkey_input, &monkeys));
         monkeys_busines.push_back(0u32)
     }
     println!("{:?}", monkey_items);
     println!("{:?}", monkeys);
-    for i in 0..20 {
+    for i in 0..10000 {
         for j in 0..monkeys.len() {
-            let monkey = &monkeys[j];
             while monkey_items[j].len() > 0 {
                 monkeys_busines[j] += 1;
-                let item = monkey_items[j].pop_front().unwrap();
-                let mut result = 
-                    if monkey.operation == Operation::Multiply {
-                        item * monkey.operand
-                    } else if monkey.operation == Operation::Add {
-                        item + monkey.operand
-                    } else if monkey.operation == Operation::Squares {
-                        item * item
+                let mut item = monkey_items[j].pop_front().unwrap();
+                for k in 0..monkeys.len() {
+                    if monkeys[j].operation == Operation::Multiply {
+                        item[k] = (item[k] * monkeys[j].operand) % monkeys[k].test;
+                    } else if monkeys[j].operation == Operation::Add {
+                        item[k] = (item[k] + monkeys[j].operand) % monkeys[k].test;
+                    } else if monkeys[j].operation == Operation::Doubles {
+                        item[k] = (item[k] * 2) % monkeys[k].test; 
                     } else {
-                        item + item
-                    };
-                result = result / 3;
-                if result % monkey.test == 0 {
-                    monkey_items[monkey.true_index as usize].push_back(result);
+                        item[k] = (item[k] * item[k]) % monkeys[k].test;
+                    }
+                }
+                if item[j] == 0 {
+                    monkey_items[monkeys[j].true_index as usize].push_back(item);
                 } else {
-                    monkey_items[monkey.false_index as usize].push_back(result);
+                    monkey_items[monkeys[j].false_index as usize].push_back(item);
                 }
 
             }
         }
-
-        println!("===== {} =====", i);
-        println!("{:?}\n", monkey_items);
-        println!("{:?}\n", monkeys_busines);
+        if i % 1000 == 0 {
+            println!("===== {} =====", i);
+            println!("{:?}\n", monkey_items);
+            println!("{:?}\n", monkeys_busines);
+        }
     }
-    format!("{:?}",monkeys_busines).to_string()
+    println!("{:?}",monkeys_busines);
+    monkeys_busines.make_contiguous().sort_by(|a, b| b.cmp(a));
+    (monkeys_busines[0] as u64 * monkeys_busines[1] as u64).to_string()
 }
 
